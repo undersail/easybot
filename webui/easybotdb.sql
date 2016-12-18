fdce565563e306a3c5988573f3bd01775f3a3966
undersail <jack@easyapple.net>
2016-12-18 20:32:15 +0800

    Change db table

--- webui/easybotdb.sql
+++ webui/easybotdb.sql
@@ -6,11 +6,11 @@ create table t_dialogs
 (
 	dialog_id int unsigned primary key not null auto_increment,
 	dialog_type varchar(10) not null,
-	dialog_time varchar(20) not null,
-	req_msg varchar(200) not null,
-	res_msg varchar(200) not null,
-	req_user varchar(20) not null,
-	res_user varchar(20) not null,
+	dialog_time int unsigned not null,
+	req_msg varchar(300) not null,
+	res_msg varchar(300) not null,
+	req_user varchar(100) not null,
+	res_user varchar(100) not null,
 	remark varchar(20) default ''
  );
 
@@ -18,10 +18,10 @@ create table t_user
 (
 	user_id int unsigned primary key not null auto_increment,
 	user_type varchar(10) not null,  
-	user_name varchar(20) not null,
+	user_name varchar(100) not null,
 	password varchar(20) default '',
 	email varchar(30) default '',
-	address varchar(100) default '',
-	avatar varchar(200) default '',
+	address varchar(300) default '',
+	avatar varchar(300) default '',
 	remark varchar(20) default ''
 );
